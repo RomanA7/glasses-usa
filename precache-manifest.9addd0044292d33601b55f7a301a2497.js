1374e74c11591eb56b9a2e49d636fd966915251d
RomanA7 <rompj7@gmail.com>
2020-10-24 22:23:21 +0300

    Updates

---
+++ precache-manifest.9addd0044292d33601b55f7a301a2497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6533b2b9c215d5e951fd3047d26e21",
+    "url": "/glasses-usa/index.html"
+  },
+  {
+    "revision": "39fc978a6f27b1392d56",
+    "url": "/glasses-usa/static/css/main.52d73dd9.chunk.css"
+  },
+  {
+    "revision": "2eed19b754cbfb67c884",
+    "url": "/glasses-usa/static/js/2.297cfa99.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/glasses-usa/static/js/2.297cfa99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fc978a6f27b1392d56",
+    "url": "/glasses-usa/static/js/main.b9464e27.chunk.js"
+  },
+  {
+    "revision": "2d4a864763f49390e4a2",
+    "url": "/glasses-usa/static/js/runtime-main.57d46790.js"
+  },
+  {
+    "revision": "a7c91a74f79e8578aae1d3e6abae16a0",
+    "url": "/glasses-usa/static/media/arrow.a7c91a74.svg"
+  }
+]);
